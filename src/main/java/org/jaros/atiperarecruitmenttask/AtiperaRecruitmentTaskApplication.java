8605d768a4383ddbefe6bc5064056fc15e6bae32
JarosStud <jaros@mat.umk.pl>
2025-07-23 17:11:30 +0200

    Add primary files

---
+++ src/main/java/org/jaros/atiperarecruitmenttask/AtiperaRecruitmentTaskApplication.java
@@ -0,0 +1,13 @@
+package org.jaros.atiperarecruitmenttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtiperaRecruitmentTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtiperaRecruitmentTaskApplication.class, args);
+	}
+
+}
